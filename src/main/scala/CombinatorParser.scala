1aaac5f93d11002735a58df97e88b7e54115e2cc
Irinel Bandas <ibandas@luc.edu>
2018-03-25 20:09:25 -0500

    added TODOs

--- src/main/scala/CombinatorParser.scala
+++ src/main/scala/CombinatorParser.scala
@@ -34,18 +34,18 @@ object CombinatorParser extends JavaTokenParsers {
   )
 
   /** statement ::= ident = expr | while (expr) statement | { statement , ... , statement } */
-  //ToDo: Add implementation of ";", conditional, and block (expression, assignment, and loop is done already)
+  //TODO: Add implementation of ";", conditional, and block (expression, assignment, and loop is done already)
   def statement: Parser[Expr] = (
     ident ~ "=" ~ expr ^^ { case s ~ _ ~ r => Assignment(Variable(s), r) }
     | "while" ~ "(" ~> expr ~ ")" ~ statement ^^ { case g ~ _ ~ b => While(g, b) }
     | "{" ~> repsep(statement, ",") <~ "}" ^^ { case ss => Sequence(ss: _*) }
   )
 
-  //assignment ::= ident "=" expression ";"
+  //TODO: assignment ::= ident "=" expression ";"
 
-  //conditional ::= "if" "(" expression ")" block [ "else" block ]
+  //TODO: conditional ::= "if" "(" expression ")" block [ "else" block ]
 
-  //loop ::= "while" "(" expression ")" block
+  //TODO: loop ::= "while" "(" expression ")" block
 
-  //block ::= "{" statement* "}"
+  //TODO: block ::= "{" statement* "}"
 }
